6460959bb7593ae2c7c1230c6b4f00c4275c0291
Ahmed Bizo <ahmedmajedb@hotmail.com>
2020-12-21 01:49:38 +0100

    made some styling to the application

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 import Todo from './Todo';
 
-const TodoList = ({todos}) => {
+const TodoList = ({ todos }) => {
 
     return (
 
         <div classNmae="todo-container">
         <ul classNmae="todo-list">
 
-          {todos.map(todo =>(
+          {todos.map (todo =>(
             < Todo key = {todo.id} text={todo.text}  />
           ) ) }
         </ul>
